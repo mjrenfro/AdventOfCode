ee712447173b5d571c1966d5e8b3cdaaf5e34cc6
Marissa <mrenfro01@gmail.com>
2017-01-21 18:01:52 -0700

    Cleaned up Pr3Part2

--- 03/part2.py
+++ 03/part2.py
@@ -1,82 +1,21 @@
-import re
+import itertools
 from itertools import islice
 
-filename='p3input.txt'
-inputDims=open(filename)
 numValid=0
-block=[]
 
-def readInDimensions():
-    global inputDims
-    nextDim=inputDims.readline().rstrip()
-    if nextDim =="":
-        return -1
-    return nextDim
-
-def readInBlocks():
-    global block
-
-
-    n=list(islice(inputDims,3))
-    tri=[x.strip().split() for x in n]
-    tri=list(zip(*tri))
-
-    block=tri
-
-def isValid(dims):
-    dims=dims.strip()
-    lengths=re.split('\s+',dims)
-    for idx,l in enumerate(lengths):
-        lengths[idx]=int(l)
-    valid=True
-    if lengths[0]+lengths[1]<=lengths[2]:
-        valid=False
-    elif lengths[1]+lengths[2]<=lengths[0]:
-        valid =False
-    elif lengths[2]+lengths[0]<=lengths[1]:
-        valid =False
-    else:
-        pass
-    return valid
-def isValid_block(tri):
-    lengths=[0]*3
-
-    for idx, l in enumerate(list(tri)):
-        lengths[idx]=int(l)
-    valid = True
-    if lengths[0]+lengths[1]<=lengths[2]:
-        valid=False
-    elif lengths[1]+lengths[2]<=lengths[0]:
-        valid =False
-    elif lengths[2]+lengths[0]<=lengths[1]:
-        valid =False
-    else:
-        pass
-    return valid
-
-
-#part 2
-def checkValidBlocks():
+def isValid(lengths):
     global numValid
-    readInBlocks()
-    while  block!=[]:
+    for pair in itertools.combinations(lengths, 2):
+        if not all(i <sum(pair) for i in lengths):
+            return
+    numValid+=1
 
-        for tri in block:
-            print()
-            if isValid_block(tri):
-                numValid+=1
+with open("p3input.txt") as f:
+    while True:
+        block=list(islice(f,3))
+        if not block: break
+        #kind of convoluted
+        block_t=list(zip(*([map(int,x.strip().split()) for x in block])))
+        for tri in block_t: isValid(tri)
 
-        readInBlocks()
-
-#part 1
-def checkValidTriangles():
-    global numValid
-    temp=readInDimensions()
-    while temp!=-1:
-        if isValid(temp):
-            numValid+=1
-        temp=readInDimensions()
-
-if __name__ == "__main__":
-    checkValidBlocks()
-    print (numValid)
+print(numValid)
