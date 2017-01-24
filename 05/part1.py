b81ebb868fca81d2f7f9a7990ad60f4c58e28c98
Marissa <mrenfro01@gmail.com>
2017-01-23 22:13:20 -0700

    cleaned up Pr5P1

--- 05/part1.py
+++ 05/part1.py
@@ -1,29 +1,15 @@
 import hashlib
 
-password=''
-
+#the provided door id 
 doorID='ugkcyxxp'
 index =0
-
-def nextChar():
-    global password
-    global index
-    #make possible new index and door id combo
+password=''
+while len(password)<8:
     seed=doorID+str(index)
     hashed=hashlib.md5(seed.encode('utf-8')).hexdigest()
-    if valid(hashed):
+    if hashed.startswith('00000'):
         password+=hashed[5]
         #optional loading presentation
         print ("------------------"+str(len(password))+"-----------------")
-
     index+=1
-
-def valid(hashed):
-        return hashed.startswith('00000')
-
-def genPassword ():
-    while len(password)<8:
-        nextChar()
-if __name__ == "__main__":
-    genPassword()
-    print (password)
+print(password)
