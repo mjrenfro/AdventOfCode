15b6f319c1ccab748d405beac872c0eaa917c4d8
mjrenfro <mrenfro01@gmail.com>
2017-01-29 21:10:49 -0700

    Pr10P1

---
+++ 10/c.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+chip1, chip2 = 17, 61
+bots = defaultdict(list)
+output = {}
+
+
+def parse(line):
+    """parse and execute a line. If not possible return False"""
+
+    cmd, *rest = line.split()
+    if cmd == 'value':
+        # value 5 goes to bot 2
+        chipnumber = int(rest[0])
+        botno = int(rest[4])
+        bots[botno].append(chipnumber)
+    if cmd == 'bot':
+        giving_botno = int(rest[0])
+        if len(bots[giving_botno]) != 2:
+            return False
+        else:
+            # bot 2 gives low to bot 1 and high to bot 0
+            botno = int(rest[5])
+            chipnumber = min(bots[giving_botno])
+            if rest[4] == 'bot':
+                bots[botno].append(chipnumber)
+            else:
+                output[botno] = chipnumber
+            botno = int(rest[10])
+            chipnumber = max(bots[giving_botno])
+            if rest[9] == 'bot':
+                bots[botno].append(chipnumber)
+            else:
+                output[botno] = chipnumber
+    return True
+
+
+with open('input.txt') as f:
+    queue = [line.rstrip('\n') for line in f.readlines()]
+    while queue:
+        line = queue.pop(0)
+        if not parse(line):
+            queue.append(line)
+
+for key, value in bots.items():
+    if chip1 in value and chip2 in value:
+        print("Bot %d has chips %d and %d" % (key, chip1, chip2))
+
+print("part B: ", output[0]*output[1]*output[2])
---
+++ 10/part1.py
@@ -0,0 +1,46 @@
+#Split into data assignment and instructions
+#Not memory efficient, but the input is so small that
+#loading all the text into lists shouldn't be an issue
+from collections import defaultdict
+bots =defaultdict(list)
+output=defaultdict(list)
+target=[17,61]
+
+chip1, chip2 = 17, 61
+def process(line):
+
+    type, *args= line.split()
+    if type=='value':
+        bots[int(args[4])].append(int(args[0]))
+        return True
+    else:
+        return process_instructions(args)
+
+#bot 129 gives low to bot 52 and high to bot 57
+def process_instructions(args):
+    curr_bot=int(args[0])
+    if len(bots[curr_bot]) != 2:
+        return False
+
+    mi=min(bots[curr_bot])
+    ma=max(bots[curr_bot])
+    if args[4]=='bot':
+        bots[int(args[5])].append(mi)
+    else:
+        output[int(args[5])]=mi
+    if args[9]=='bot':
+        bots[int(args[10])].append(ma)
+    else:
+        output[int(args[10])]=ma
+    return True
+
+with open('input.txt') as f:
+    lines= [l.strip() for l in f.readlines()]
+    while lines:
+        for l in lines:
+            if process(l):
+                lines.remove(l)
+
+for key, value in bots.items():
+    if target== sorted(value):
+        print("Bot ",key)
