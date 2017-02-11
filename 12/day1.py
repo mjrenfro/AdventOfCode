e2e34fd9da7945e836e5ba60b60d40395d38cdd6
mjrenfro <mrenfro01@gmail.com>
2017-02-10 23:17:07 -0700

    Added D12P1

---
+++ 12/day1.py
@@ -0,0 +1,26 @@
+registers ={"a":0, "b":0, "c":0, "d":0}
+code=[]
+
+def process_line(idx):
+    while(idx<len(code)):
+        cmd, *args=code[idx].split()
+        offset=1
+        if cmd== "cpy":
+            registers[args[1]]=int(args[0]) if args[0].isdigit() else registers[args[0]]
+        elif cmd=="inc":
+            registers[args[0]]+=1
+        elif cmd=="dec" :
+            registers[args[0]]-=1
+        elif cmd=="jnz":
+            if (args[0]=='b'):
+                print(registers[args[0]])
+            comp= int(args[0]) if args[0].isdigit() else registers[args[0]]
+            if comp!=0:
+                offset=int(args[1])
+        idx+=offset
+        print(registers)
+
+with open ('input.txt') as f:
+    code=[l.rstrip('\n') for l in f.readlines()]
+    process_line(0)
+    print(registers)
