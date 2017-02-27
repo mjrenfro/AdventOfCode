e163b3fa85920659e0ca0099fdda89d2ccbbd5d5
mjrenfro <mrenfro01@gmail.com>
2017-02-27 15:22:59 -0700

    Added Pr13P2

--- 13/day1.py
+++ 13/day1.py
@@ -3,15 +3,15 @@ frontier=[]
 key=1364
 
 goal=(31,39)
+visited=set()
+partB=True
 
 #got most logic for this func from SO
 def get_parity(n):
     c=0
     while n:
-
         c+=1
         n &= n-1
-
     return not bool(c%2)
 
 #Returns the manhattan distance between two points
@@ -48,9 +48,10 @@ def get_neighbors(point):
     for o in offsets:
         #here is where NumPy would make this look better
         neighbors.append(tuple(sum(x) for x in zip(point, o)))
-    print (neighbors)
+    # print (neighbors)
     return neighbors
 
+#for efficiently determining the path
 def a_star():
     start=(1,1)
     heapq.heappush(frontier, (0,(1,1)) )
@@ -60,20 +61,35 @@ def a_star():
     cost_so_far[start] = 0
 
     while not frontier==[]:
-        print (cost_so_far)
         current=heapq.heappop(frontier)[1]
-        print (current)
 
         if current==goal:
-            print ("Cost so far ", cost_so_far)
             return cost_so_far[current]
 
         for neig in [n for n in get_neighbors(current) if is_walkable(n)]:
-            print("neig: ", neig)
             new_cost=cost_so_far[current] + 1
             if neig not in cost_so_far or new_cost<cost_so_far[neig]:
                 cost_so_far[neig]=new_cost
                 priority=new_cost+find_dist(goal, neig)
                 heapq.heappush(frontier, (priority,neig) )
                 came_from[neig]=current
-print(a_star())
+
+#for determining the unique nodes at most 50 nodes away.
+#Python isn't the greatest for recursive; so using a queue
+#gonna steal this tuple, length idea from the amazing tomkooij :
+#https://github.com/tomkooij/AdventOfCode/blob/master/aoc2016/day13.py
+def breadth_first(path_head):
+    queue=[path_head]
+    while queue:
+        dist,node=queue.pop(0)
+        if node not in visited:
+            visited.add(node)
+            if dist ==50:
+                return
+            for neig in [n for n in get_neighbors(node) if is_walkable(n)]:
+                queue.append((dist+1, neig))
+
+print("Length: ",a_star())
+if partB:
+    breadth_first((0,(1,1)))
+    print("Num nodes < 50: ",len(visited))
